46f4352302b4acbed1a31dc1cd9949f8a6f1c856
Umang Patel <umangpatel1991.cmail.com>
2021-09-22 14:56:35 -0400

    experimented with moes(Move: up) right after connecting to server

--- play.js
+++ play.js
@@ -8,6 +8,20 @@ conn.setEncoding("utf8");
 
 conn.write("Name: upp");
 
+/* conn.write("Move: up");
+
+const snakeMoveUp = setInterval(() => {
+  conn.write("Move: up");
+}, 300);
+
+for (let i = 0; i <= 5; i++) {
+  setTimeout(() => {
+    console.log(i);
+    if (i === 5)
+      clearInterval(snakeMoveUp);
+  }, 300 * i);
+} */
+
 conn.on('data', (data) => {
   console.log("DATA CAME IN!!!!!");
   console.log(data);
