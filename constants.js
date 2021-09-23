b1de5657907d157cea023d358dede1cff7775ab5
Umang Patel <umangpatel1991.cmail.com>
2021-09-22 20:27:58 -0400

    refactoring code by adding hardcoded values to constants lke movement keys, snake messages

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = '135.23.223.133';
+const PORT = '50542';
+const MOVE_UP_KEY = 'W';
+const MOVE_DOWN_KEY = 'S';
+const MOVE_LEFT_KEY = 'A';
+const MOVE_RIGHT_KEY = 'D';
+const MESSAGES = {
+  'H': "HELLO",
+  'G': "GOOD",
+  'F': "FINE"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES
+};
--- input.js
+++ input.js
@@ -1,5 +1,7 @@
+const {MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGES} = require('./constants');
 // Stores the active TCP connection object.
 let connection;
+const msgKeys = Object.keys(MESSAGES);
 
 // setup interface to handle user input from stdin
 const setupInput = (conn) => {
@@ -12,22 +14,22 @@ const setupInput = (conn) => {
 };
 
 const handleUserInput = (key) => {
+  
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
   } else if (key === '\u2386') {
     console.log("hello");
-  } else if (key === 'w') {
+  } else if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
-  } else if (key === 's') {
+  } else if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
-  } else if (key === 'a') {
+  } else if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
-  } else if (key === 'd') {
+  } else if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
-  } else {
-    connection.write(`Say: ${key}`);
-    process.stdout.write(key);
+  } else if (msgKeys.includes(key)) {
+    connection.write(`Say: ${MESSAGES[key]}`);
   }
   //process.stdout.write(key);
 
